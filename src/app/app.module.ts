ef1149f966cba47a31c0eee6009e67da6ebaead2
alfredolcontrerasl@gmail.com <alfredolcontrerasl@gmail.com>
2019-07-24 21:42:50 -0400

    angular task

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+
+import { routing, appRoutingProviders } from './app.routing';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './component/login.component';
+import { RegisterComponent } from './component/register.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    routing
   ],
-  providers: [],
+  providers: [
+    appRoutingProviders
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/component/login.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute, Params } from '@angular/router';
+
+@Component({
+    selector: 'login',
+    templateUrl: '../views/login.html'
+})
+export class LoginComponent implements OnInit {
+    public title: string;
+
+    constructor(
+        //private _route: ActivatedRoute,
+       // private _router :Router
+    ){
+        this.title = 'Componente de login';
+    }
+
+    ngOnInit(){
+        console.log("el componente login ha sido cargado correctamente.")
+    }
+}

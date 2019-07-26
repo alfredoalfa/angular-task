8c93d994f720fe485391990d43be60794a145c30
alfredo <alfredolcontrerasl@gmail.com>
2019-07-26 18:14:15 -0400

    Services user

--- src/app/component/login.component.ts
+++ src/app/component/login.component.ts
@@ -1,22 +1,35 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute, Params } from '@angular/router';
-import { NgForm } from '@angular/forms';
+import { UserService } from '../services/user.services'; 
 
 @Component({
     selector: 'login',
-    templateUrl: '../views/login.html'
+    templateUrl: '../views/login.html',
+    providers: [UserService]
 })
 export class LoginComponent implements OnInit {
     public title: string;
+    public user;
 
     constructor(
-        //private _route: ActivatedRoute,
-       // private _router :Router
+        private _route: ActivatedRoute,
+        private _router: Router,
+        private _UserService: UserService
     ){
         this.title = 'Identificate';
+        this.user = {
+            "email":"",
+            "password":"",
+            "gethash": "false"
+        };
     }
 
     ngOnInit(){
         console.log("el componente login ha sido cargado correctamente.")
+    }
+
+    onSubmit(){
+        console.log(this.user);
+        alert(this._UserService.signup());
     }
 }
---
+++ src/app/services/global.ts
@@ -0,0 +1,3 @@
+export var GLOBAL = {
+    url: 'http://http://localhost/curso-backfront/web/app_dev.php'
+};
